a4792e33c7543ee334196914dcf02f6693d11906
carlos <carlosjavi1887@gmail.com>
2026-05-19 17:04:18 -0500

    feat(security): implement UserDetailsService with roles and permissions loading and add BCrypt password encoder

---
+++ security-module/src/main/java/com/carlos/security/core/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,47 @@
+package com.carlos.security.core.infrastructure.security;
+
+import com.carlos.security.core.domain.model.User;
+import com.carlos.security.core.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        user.getRoleNames().forEach(role ->
+                authorities.add(new SimpleGrantedAuthority(role))
+        );
+
+        user.getAllPermissions().forEach(permission ->
+                authorities.add(new SimpleGrantedAuthority(permission.getName()))
+        );
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword().getValue())
+                .accountExpired(!user.isAccountNonExpired())
+                .accountLocked(user.isAccountLocked())
+                .credentialsExpired(!user.isCredentialsNonExpired())
+                .disabled(!user.isEnabled())
+                .authorities(authorities)
+                .build();
+    }
+}
